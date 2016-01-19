966fe54998dd55332905e53c6f61e2c16ab8ab71
Wes Chow <wes@chartbeat.com>
2016-01-19 10:36:32 -0500

    Get setup.py ready for pypi.

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 from setuptools import setup
 
 setup(
-    name='swailing',
-    version='0.1',
-    packages=['swailing'],
+    name="swailing",
+    version="0.1",
+    author="Wes Chow",
+    author_email="wes@chartbeat.com",
+    description=("Logging library for apps that produce debilitating amounts of log output."),
+    license="Apache",
+    keywords="logging",
+    url="https://github.com/chartbeat-labs/swailing",
+    py_modules=['swailing'],
+    long_description="""
+An opinionated logging library for applications that produce a debilitating amount of log output.
+
+Swailing uses the token bucket algorithm and provides facilities for
+PostgreSQL style error logs. See
+https://github.com/chartbeat-labs/swailing for more details.
+""",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Topic :: System :: Logging",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Apache Software License",
+    ],
 )
